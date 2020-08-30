e817ac282da39a349f2d17f856083bdecba23b61
unknown <leductrung142@gmail.com>
2020-08-30 17:13:35 +0200

    test

---
+++ test30-8.py
@@ -0,0 +1,53 @@
+# Exo1-1
+# exo1 = input("Nhập chuỗi:")
+# max1= exo1[0]
+# min1 = exo1[0]
+# for i in exo1:
+#   if i>max1:
+#     max1=i
+#   elif i<min1:
+#     min1=i
+
+# print(f"ký tự max là {max1} và ký tự min là {min1}")
+
+# Exo1-2
+# n = int(input("Nhập số nguyên dương:"))
+# sum=0
+# temp=0
+# while n < 0:
+#   n = int(input("Nhap số nguyên dương "))
+# while (n>0):
+#   sum += n%10
+#   n = int(n/10)
+# print(sum)
+
+#Exo1-3
+
+
+
+# Exo2
+# n = int(input("Nhập số điện tiêu thụ:"))
+# total=0
+# while n<0:
+#   n = int(input("Nhập số điện tiêu thụ:"))
+# if 0<=n<=100:
+#   total = n*1500
+# elif 100<n<=300:
+#   total = n*2000
+# else:
+#   total = n*3000
+
+# print(f"số tiền cần trả là {total} VNĐ")
+
+# Exo3: tìm số nguyên tố nhỏ hơn n
+# n = int(input("Nhap số nguyên n "))
+
+# if n<2:
+#   print(f"không tồn tại số nguyên tố nhỏ hơn {n}")
+# for i in (2,n):
+
+
+
+
+
+
